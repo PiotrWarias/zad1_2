83669f48a8e844bb89090c9238106a9a632ebd23
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-03-15 12:05:55 +0000

    poprawa na gotowe dane

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
-    Scanner scan = new Scanner(System.in);
-    int x = scan.nextInt();
+    int x = 100;
     System.out.println(x);
   }
 }
